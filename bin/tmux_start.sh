4384aac4c38494a5714a15177ad86a8bf2daf723
Jonathan Shapiro <shapiroj18@gmail.com>
2021-10-05 15:46:11 -0400

    updated script to go to left shell for easy entry

--- bin/tmux_start.sh
+++ bin/tmux_start.sh
@@ -2,22 +2,12 @@
 
 color -r
 
-# creates new tmux server according to stack overflow article below
-# https://stackoverflow.com/questions/57429593/create-new-pane-from-tmux-command-line
-
-# tmux start-server
-
-# create a session with three panes
-# tmux new-session -s art -d "/usr/bin/env sh -c \"clear\"; /usr/bin/env sh -i"
-# tmux split-window -h art:0 "/usr/bin/env sh -c \"cbonsai -i --wait=1 -l\"; /usr/bin/env sh -i"
-# tmux split-window -t art:0 "/usr/bin/env sh -c \"asciiquarium\"; /usr/bin/env sh -i"
-
-
 tmux new-session -d -s 'art'\; \
 	split-window -h \; \
 	send-keys 'cbonsai --infinite --live --wait=1' C-m \; \
 	split-window -v \; \
-	send-keys 'asciiquarium' C-m \;
+	send-keys 'asciiquarium' C-m \; \
+	select-pane -t 0 \;
 
 tmux attach -t art
 
